aba0d741ceb0657681d8f746576ba5dbe16f0c02
Marlonsouto <soutomarlon@gmail.com>
2024-11-29 22:00:12 -0300

    fix(App): Corrigindo falha na importaçao da Footer para App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/header/Header.jsx';
-import Footer from './components/header/footer/Footer.jsx';
+import Footer from './components/footer/Footer.jsx';
 import './globalStyle/globalStyle.scss';
 
 
