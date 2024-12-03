4a5437fb5175f5e2c16cb1f5bd20c302623ab56c
Marlonsouto <soutomarlon@gmail.com>
2024-12-03 19:06:55 -0300

    feat(queroDoar): Adicionando html de formulario  ao componente

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,7 +1,18 @@
 export default function QueroDoar(){
     return (
       <section>
-        <h1>EU SOU A QUERO DOAR</h1>
+        <h1>Por favor, preencha o formulário com suas informações e as informações do Livro</h1>
+
+        <section>
+          <form>
+            <input type="text" name="titulo" id="" placeholder="Titulo"/>
+            <input type="text" name="categoria" id="" placeholder="Categoria"/>
+            <input type="text" name="autor" id="" placeholder="Autor"/>
+            <input type="url" name="LinkImagem" id="" placeholder="Link Da Imagem"/>
+            <input type="submit" value="" placeholder="Doar" />
+          </form>
+        </section>
+        
       </section>
     )
   
