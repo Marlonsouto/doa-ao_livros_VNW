561df71fd2c57b1fc1763659937911817f279cb8
Marlonsouto <soutomarlon@gmail.com>
2024-12-03 20:20:37 -0300

    Feat(queroDoar): implementando style do formulario

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,19 +1,27 @@
+import styleQueroDoar from './queroDoar.module.scss'
+import livroazul from '../../assets/icones_queroDoar/livrodoForm.png'
+
 export default function QueroDoar(){
     return (
-      <section>
+      <section className= {styleQueroDoar.conteinerQuerodoar}>
         <h1>Por favor, preencha o formulário com suas informações e as informações do Livro</h1>
 
         <section>
-          <form>
+
+          <form className={styleQueroDoar.formulario}>
+            
+            <img src={livroazul} alt="Desenho de um livro" />
+            
             <input type="text" name="titulo" id="" placeholder="Titulo"/>
             <input type="text" name="categoria" id="" placeholder="Categoria"/>
             <input type="text" name="autor" id="" placeholder="Autor"/>
-            <input type="url" name="LinkImagem" id="" placeholder="Link Da Imagem"/>
+            <input type="url" name="LinkImagem" id="" placeholder="Link da imagem"/>
             <input type="submit" value="" placeholder="Doar" />
           </form>
         </section>
-        
+
       </section>
     )
-  
+    
   }
+  //<h2>Informações do Livro</h2>
