3f102f683c8930d39a0f6921bc83fc7a48a45e29
Marlonsouto <soutomarlon@gmail.com>
2024-12-03 20:48:33 -0300

    Feat(queroDoar): ajustando style do formulario

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,27 +1,21 @@
 import styleQueroDoar from './queroDoar.module.scss'
 import livroazul from '../../assets/icones_queroDoar/livrodoForm.png'
 
-export default function QueroDoar(){
-    return (
-      <section className= {styleQueroDoar.conteinerQuerodoar}>
-        <h1>Por favor, preencha o formulário com suas informações e as informações do Livro</h1>
-
-        <section>
-
-          <form className={styleQueroDoar.formulario}>
-            
-            <img src={livroazul} alt="Desenho de um livro" />
-            
-            <input type="text" name="titulo" id="" placeholder="Titulo"/>
-            <input type="text" name="categoria" id="" placeholder="Categoria"/>
-            <input type="text" name="autor" id="" placeholder="Autor"/>
-            <input type="url" name="LinkImagem" id="" placeholder="Link da imagem"/>
-            <input type="submit" value="" placeholder="Doar" />
-          </form>
-        </section>
-
-      </section>
-    )
+export default function QueroDoar() {
+  return (
+    <section className={styleQueroDoar.conteinerQuerodoar}>
+      <h1>Por favor, preencha o formulário com suas informações e as informações do Livro</h1>
+      <form className={styleQueroDoar.formulario}>
+        <img src={livroazul} alt="Desenho de um livro" />
+        <input type="text" name="titulo" id="" placeholder="Titulo" />
+        <input type="text" name="categoria" id="" placeholder="Categoria" />
+        <input type="text" name="autor" id="" placeholder="Autor" />
+        <input type="url" name="LinkImagem" id="" placeholder="Link da imagem" />
+        <input className={styleQueroDoar.submit} type="submit" value="" placeholder="Doar" />
+      </form>
     
-  }
-  //<h2>Informações do Livro</h2>
+    </section>
+  )
+
+}
+//<h2>Informações do Livro</h2>
